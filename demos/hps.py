2df6796b9228c24b52084d362c0ba12ba009a0f6
Caroline <huang244@msu.edu>
2025-03-09 16:14:12 -0400

    Update hps.py
    hi

--- demos/hps.py
+++ demos/hps.py
@@ -25,20 +25,27 @@ duration = 2
 print('starting recording!')
 audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1) # sd.rec returns a numpy array
 sd.wait # wait until the recording is done to start the next step
-freq_spectrum = scipy.fftpack.fft(audio_data) # this returns an array of complex numbers
+full_freq_spectrum = (scipy.fftpack.fft(audio_data)) # this returns an array of complex numbers
+freq_spectrum = full_freq_spectrum[:len(full_freq_spectrum)//2] # just take the first half bc its mirrored (idk why)
 
-# COMPLEX NUMBERS AND FOURIER TRANSFORM CRASH COURSE (this explanation sux. sorry.)
-   # if you want to understand this more, look at proofs for Taylor series, Euler's identity,
-   # trig identities, the complex plane, and Fourier series. i dont really get it entirely tbh lol.
-# : z = a + jb
-# A complex number has a complex component (b) and a real component (a)
-# Magnitude at that frequency is given by sqrt(a**2 + b**2)
-# The array is sorted by frequency. (not a math formula). the frequency is fs*k/N where k is the index
-# Phase, aka shift from sine, is given by arctain(b/a)
-# the fft also gives a mirrored image of complex conjugates so we only need to look at one half of the array
-# Fourier transform is a way to identify which frequencies are contained in a periodic time domain signal
-# fft is the fast fourier transform it uses polynomials or something to do the fourier transform faster idk
+
+
+
 
 
 
 print('done')
+
+# COMPLEX NUMBERS AND FOURIER TRANSFORM CRASH COURSE (this explanation sux. sorry.)
+   # if you want to understand this more, look at proofs for Taylor series, Euler's identity,
+   # trig identities, the complex plane, and Fourier series. i dont really get it entirely tbh lol.
+
+# Complex number: z = a + jb
+# A complex number has a complex component (b) and a real component (a). j is the imaginary number sqrt(-1)
+# complex number contains a lot of important info. it tells us the magnitude& phase of a certain freq component.
+# Magnitude at that frequency is given by sqrt(a**2 + b**2)
+# Phase, aka shift from sine, is given by arctain(b/a)
+# The array is sorted by frequency. (not a math formula). the frequency is fs*k/N where k is the index
+# Fourier transform is a way to identify which frequencies are contained in a periodic time domain signal
+# fft is the fast fourier transform it uses polynomials or something to do the fourier transform faster idk
+# the fft gives a symettric array of complex conjugates so we only need to look at one half of the array
