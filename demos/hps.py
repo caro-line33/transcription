b00f0c2cbf1f7b7289a02d0e0a417e8c3bdbe718
Caroline <huang244@msu.edu>
2025-03-20 00:24:49 -0400

    hi
    hi

--- demos/hps.py
+++ demos/hps.py
@@ -15,22 +15,28 @@ def callback(indata, frames, time_info, status):
         return
     full_frequency = scipy.fftpack.fft(indata.flatten())
     frequency = full_frequency[:len(full_frequency)//2]
+    freq_res = sample_freq/window_size
+    max_downsampling = 5
+    new_res = freq_res/max_downsampling
     magnitude = np.abs(frequency)
     original_indices = np.arange(len(magnitude))  
-    more_indices = np.arange(0, len(magnitude), 1/5)
+    more_indices = np.arange(0, len(magnitude), new_res)
     magnitude_interp = np.interp(more_indices, original_indices, magnitude)
+    hps_spec = magnitude_interp.copy()
 
-
-    max_mag = np.argmax(magnitude)
-    bin_range = sample_freq/window_size
-    dominant_frequency = max_mag*bin_range+(bin_range/2)
-
+    for i in range(2, (max_downsampling+1)):  
+      downsampled = magnitude_interp[::i]
+      min_length = min(len(hps_spec), len(downsampled))
+      hps_spec = hps_spec[:min_length] * downsampled[:min_length]
     
+    max_ind = np.argmax(hps_spec)
+    fundamental_frequency = max_ind * new_res 
     os.system('cls' if os.name == 'nt' else 'clear')
+    print(fundamental_frequency)
     
 try:
     with sd.InputStream(channels=1, callback=callback, samplerate=sample_freq, blocksize=window_size):
         while True:
-            time.sleep(0.1)
+            time.sleep(0.01)
 except Exception as e:
     print(e)
--- demos/multiple_notes.py
+++ demos/multiple_notes.py
@@ -1,62 +1,42 @@
-import sounddevice as sd
+import os
 import numpy as np
-import scipy.fftpack  # scipy fft is faster than numpy fft
+import scipy.fftpack
+import sounddevice as sd
+import time
+from collections import deque
 
-fs = 44800  # sampling frequency
-duration = 2  # seconds
-harmonics = 5  # Number of harmonics to use for HPS
+sample_freq = 48000 # number of samples taken per second
+window_size = 48000
 
-# Recording audio
-print('Starting recording...')
-audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1)  # sd.rec returns a numpy array
-sd.wait()  # wait until the recording is done to start the next step
-print('Recording finished.')
 
-# Perform FFT to get frequency spectrum
-freq_spectrum = scipy.fftpack.fft(audio_data.flatten())  # Flatten to convert to 1D array
-magnitude_spectrum = np.abs(freq_spectrum)[:len(freq_spectrum)//2]  # Use just the first half.
+def callback(indata, frames, time_info, status):
+    if status:
+        print(status)
+        return
+    full_frequency = scipy.fftpack.fft(indata.flatten())
+    frequency = full_frequency[:len(full_frequency)//2]
+    freq_res = sample_freq/window_size
+    max_downsampling = 5
+    new_res = freq_res/max_downsampling
+    magnitude = np.abs(frequency)
+    original_indices = np.arange(len(magnitude))  
+    more_indices = np.arange(0, len(magnitude), new_res)
+    magnitude_interp = np.interp(more_indices, original_indices, magnitude)
+    hps_spec = magnitude_interp.copy()
 
-# Harmonic Product Spectrum (HPS) Calculation
-hps_spectrum = magnitude_spectrum.copy()
-for i in range(1, harmonics):
-    # Downsample and multiply the spectrums together
-    scaled_spectrum = np.interp(np.arange(len(hps_spectrum)), np.arange(0, len(hps_spectrum), i + 1), magnitude_spectrum[::(i + 1)])
-    hps_spectrum *= scaled_spectrum
-
-# Function to detect peaks
-def detect_peaks(spectrum, threshold_ratio=0.5):
-    detected_peaks = []
-    while True:
-        peak_freq = np.argmax(spectrum)  # Find index of peak magnitude
-        peak_value = spectrum[peak_freq]
-
-        if peak_value == 0:
-            break
-
-        peak_frequency = peak_freq * (fs / len(audio_data))  # Convert index to frequency
-        detected_peaks.append((peak_frequency, peak_value))
-
-        # Set the peak to zero to avoid detecting the same peak again
-        spectrum[peak_freq] = 0
-
-        # Find the second highest peak
-        second_peak_spectrum = np.copy(spectrum)
-        second_peak_spectrum[peak_freq] = 0
-        second_peak_freq = np.argmax(second_peak_spectrum)
-        second_peak_value = second_peak_spectrum[second_peak_freq]
-        
-        # Check if the second peak is significant (e.g., > half of the first peak)
-        if second_peak_value > peak_value * threshold_ratio:
-            continue
-        else:
-            break
-
-    return detected_peaks
-
-# Detect peaks in the HPS spectrum
-detected_peaks = detect_peaks(hps_spectrum, threshold_ratio=0.5)
-
-# Print the detected frequencies and their magnitudes
-print("Detected frequencies and magnitudes:")
-for freq, value in detected_peaks:
-    print(f"Frequency: {freq} Hz, Magnitude: {value}")
+    for i in range(2, (max_downsampling+1)):  
+      downsampled = magnitude_interp[::i]
+      min_length = min(len(hps_spec), len(downsampled))
+      hps_spec = hps_spec[:min_length] * downsampled[:min_length]
+    
+    max_ind = np.argmax(hps_spec)
+    fundamental_frequency = max_ind * new_res 
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(fundamental_frequency)
+    
+try:
+    with sd.InputStream(channels=1, callback=callback, samplerate=sample_freq, blocksize=window_size):
+        while True:
+            time.sleep(0.01)
+except Exception as e:
+    print(e)
