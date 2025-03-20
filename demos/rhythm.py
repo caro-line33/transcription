04bcf2ab532985364d4a04c972192daedae13f2f
Caroline <huang244@msu.edu>
2025-03-20 01:04:15 -0400

    h
    hi

--- demos/rhythm.py
+++ demos/rhythm.py
@@ -1,11 +1,4 @@
-''' 
-idea: have a buffer note which is the prev note, whenever current note != buffer, add a tuple which is 
-(prev note, buffer) to the array and reset the timer and buffer
 
-or triple since multiple notes could happen at once. so whenver a new one is detected, add a triple
-which is (note, note_start, note_length) to the array.
-
-'''
 
 
 
